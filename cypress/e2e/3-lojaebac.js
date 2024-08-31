f8fcab29f12edaa5840506053a987a4e9bc723a5
Diego <droxo63@gmail.com>
2024-08-31 14:31:59 -0300

    primeiro teste

---
+++ cypress/e2e/3-lojaebac.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+
+describe('example to-do app', () => {
+   
+    it("Teste de login"), () => {
+        cy.visit('https://example.cypress.io/todo')
+        
+    }
+      
+    })
