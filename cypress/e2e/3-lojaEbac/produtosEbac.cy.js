28b1ba33180aa223c6f051846d45d71c9e2b2df4
Diego <droxo63@gmail.com>
2024-09-23 21:20:08 -0300

    implantação produtosPage na pagina produtosEbac com massa de dados fixa

--- cypress/e2e/3-lojaEbac/produtosEbac.cy.js
+++ cypress/e2e/3-lojaEbac/produtosEbac.cy.js
@@ -1,52 +1,40 @@
 /// <reference types="cypress" />
-//import { produtosPage } from "../../support/pageObjects/produtosPage";
+
+import produtosPage from "../../support/pageObjects/produtosPage"
+
+
 describe('Funcionalidade de Produtos', () => {
+beforeEach(() => {
+    produtosPage.visitarUrl()
+});
 
-    beforeEach(() => {
-      // produtosPage.visitarUrl()
-      cy.visit('produtos/')
+    it('Deve visitar a pagina do produto', () => {
+        cy.visit("aether-gym-pant/")
+        cy.get('.product_title').contains("Aether Gym Pant")
+        
     });
+    it('Deve selecionar o produto com sucesso', () => {
+       
+        produtosPage.selecionarProduto("Abominable Hoodie")
+        cy.get('.product_title').contains("Abominable Hoodie")
 
-    //afterEach(() => {
-     //   cy.screenshot()
-    //});
+    });
+    it("Deve buscar o produto com sucesso", ()=>{
+       
+        produtosPage.buscarProduto("Aero Daily Fitness Tee")
+        cy.get('.product_title').contains("Aero Daily Fitness Tee")
+    })
 
-it('Deve selecionar um produto da lista', () => {
-    cy.get('.products > .row')
-    //.eq(2)
-    .contains("Aero Daily Fitness Tee")
-    .click()
-    cy.get('.product_title').should("contain", "Aero Daily Fitness Tee")
-    
+    it('Deve adicionar produto no carrinho', () => {
+        produtosPage.buscarProduto("Aether Gym Pant")
+        cy.get('.button-variable-item-36').click()
+        cy.get('.button-variable-item-Green').click()
+        cy.get('.input-text').clear().type(2)
+        cy.get('.single_add_to_cart_button').click()
+        cy.get('.woocommerce-message').contains("2 × “Aether Gym Pant” foram adicionados no seu carrinho.")
 
-});
 
-it('Deve fazer busca de produto (campo de busca)', () => {
-    //produtosPage.buscarProduto("")
-    cy.get("[type='text']").eq(1).type("Aero Daily Fitness Tee")
-    cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
-    cy.get('.product_title').should("contain", 'Aero Daily Fitness Tee')
-});
-it('Deve visitar a pagina do produto', () => {
-   // produtosPage.visitarProduto("")
-   cy.visit(`produtos/${"Aero-Daily-Fitness-Tee"}`)
-   cy.get('.product_title').should("contain", 'Aero Daily Fitness Tee')
-});
 
-it.only('Deve adicionar produto no carrinho', () => {
-   //produtosPage.adicionarCarrinho("")
-   let qtd = 7
-   let tamanho = "M"
-   let cor = "Blue" 
-    let produto = "Atlas Fitness Tank"
-   cy.get("[type='text']").eq(1).type(produto)
-   cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
-   cy.get('.product_title').should("contain", produto)
-   cy.get(`.button-variable-item-${tamanho}`).click()
-   cy.get(`.button-variable-item-${cor}`).click()
-   cy.get('.input-text').clear().type(qtd)
-   cy.get('.single_add_to_cart_button').click()
-   cy.get('.woocommerce-message').should("contain", `${qtd} × “Atlas Fitness Tank” foram adicionados no seu carrinho.`)
-});
-
+        
+    });
 })
--- cypress/support/pageObjects/produtosPage.js
+++ cypress/support/pageObjects/produtosPage.js
@@ -1,24 +1,37 @@
-class Produtos {
+class ProdutosPage {
     visitarUrl() {
-    cy.visit('produtos/')
+    cy.visit('produtos')
     }
 
-    selecionarProduto() {
-
+    selecionarProduto(nomeProduto) {
+        cy.get('.products > .row').should("exist")
+       // cy.get("[title='Abominable Hoodie']")
+       cy.get(".block-inner").first()
+       
+        .click()
     }
 
     buscarProduto(nomeProduto) {
-        cy.get('.products > .row')
-        .contains(nomeProduto)
-        .click()    
+        cy.get('.page-title').contains("Produtos")
+        cy.get("[type='text']").eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
+        
+
     }
-
-    visitarProduto() {
-
+    
+    visitarProduto(nomeProduto) {
+        cy.visit(`produtos/${nomeProduto}`)
     }
     adicionarCarrinho() {
+        cy.get('.post-2559 > .product-block > .block-inner > .image > .product-image > .image-hover').click()
+        cy.get('.search > .tbay-search-form > .form-ajax-search > .form-group > .input-group > .button-group > .button-search').click()
+    }
 
+    buscarProduto2(nomeProduto) {
+        cy.get('.page-title').contains("Produtos")
+        cy.get("[type='text']").eq(2).type(nomeProduto)
+        cy.get('.button-search').eq(2).click()
     }
 }
 
-export default new Produtos()
+export default new ProdutosPage()
